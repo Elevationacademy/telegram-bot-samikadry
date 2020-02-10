2aa898fc5e3b4bb85ea3e6bfe21daf4cae86a6cb
samikadry <samikadry9@gmail.com>
2020-02-10 15:40:50 +0200

    Telegram Bots complete program

--- server.py
+++ server.py
@@ -14,6 +14,28 @@ TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
 
+def isPrime(num):
+    if num > 1:
+        for i in range(2, (num // 2)+1):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def isFactorial(num):
+    fact = i = 1
+    while i <= num:
+        fact *= i
+        i += 1
+        if fact == num:
+            message = f"{num} is a result of factorial operation"
+            return True
+    return False
+
+
 @app.route('/message', methods=["POST"])
 def handle_message():
     message = " "
@@ -21,43 +43,40 @@ def handle_message():
     chat_id = message_info['message']['chat']['id']
     command = message_info['message']['text']
     res = command.split()
-    num = int(res[1])
-    if res[0] == "check":
-        if num > 1:
-            for i in range(2, num // 2):
-                if (num % i) == 0:
-                    message = f"{num} is not a prime number"
-                    break
-            else:
+    try:
+        num = int(res[1])
+        if res[0] == "check":
+            if isPrime(num):
                 message = f"{num} is a prime number"
-        else:
-            message = f"{num} is not a prime number"
+            else:
+                message = f"{num} isn't a prime number"
 
-    elif res[0] == "factorial":
-        message = f"{num} isn't a result of factorial operation"
-        fact = i = 1
-        while i <= num:
-            fact *= i
-            i += 1
-            if fact == num:
+        elif res[0] == "factorial":
+            if isFactorial(num):
                 message = f"{num} is a result of factorial operation"
-                break
+            else:
+                message = f"{num} isn't a result of factorial operation"
 
-    elif res[0] == "palindrome":
-        if str(num) == str(num)[::-1]:
-            message = f"{num} is PALINDROME"
+        elif res[0] == "palindrome":
+            if str(num) == str(num)[::-1]:
+                message = f"{num} is PALINDROME"
+            else:
+                message = f"{num} isn't PALINDROME"
+
+        elif res[0] == "sqrt":
+            root = math.sqrt(num)
+            if int(root + 0.5) ** 2 == num:
+                message = f"{num} have an integer square root"
+            else:
+                message = f"{num} haven't an integer square root"
+
         else:
-            message = f"{num} isn't PALINDROME"
-
-    elif res[0] == "sqrt":
-        root = math.sqrt(num)
-        if int(root + 0.5) ** 2 == num:
-            message = f"{num} have an integer square root"
-        else:
-            message = f"{num} haven't an integer square root"
-
-    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id, message))
-    return Response("success")
+            message = f"Invalid command {res[0]}!!\n"
+    except:
+        message = f"Invalid input, must be a number after the command.\n"
+    finally:
+        requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id, message))
+        return Response("success")
 
 
 if __name__ == '__main__':
